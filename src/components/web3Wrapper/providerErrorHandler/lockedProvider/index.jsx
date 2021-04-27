47bda0d2b57bddb11ad7fbb1b7ef137f3e5d3ed6
ux33-331 <61588635+ux33-331@users.noreply.github.com>
2021-04-27 13:26:00 +0200

    Placeholder

--- src/components/web3Wrapper/providerErrorHandler/lockedProvider/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const LockedProvider = () => (
-  <h1>Locked provider</h1>
-);
-
-export default LockedProvider;
--- src/contexts/snowflakeContext.jsx
+++
@@ -1,7 +0,0 @@
-import {
-  createContext,
-} from 'react';
-
-const SnowflakeContext = createContext();
-
-export default SnowflakeContext;
